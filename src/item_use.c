a63ae4d33a4e0903b177ba592875aef0e1e1d5d9
Acimut <79547805+Acimut@users.noreply.github.com>
2022-04-07 00:34:12 -0500

    Update item_use.c

--- src/item_use.c
+++ src/item_use.c
@@ -42,43 +42,90 @@
 #include "constants/item_effects.h"
 #include "constants/items.h"
 #include "constants/songs.h"
+#include "quest_log.h"
 
+
+/**
+ * ::ACIMUT::
+ * 2022/04/06
+ * 
+ * - Esta función en FR se conoce como:
+ *      void FieldUseFunc_MachBike(u8 taskId)
+ * - Cambio de función correspondiente a fire red.
+ * - No es llamada en otra parte de la inyección.
+ * - Es posible hacer hook a toda esta función.
+ * - Usado en items.
+ */
+
+//static EWRAM_DATA 
+extern void (*sItemUseOnFieldCB)(u8 taskId);// = NULL;
+
+//static
+extern void DisplayItemMessageInCurrentContext(u8 taskId, bool8 inField, u8 textSpeed, const u8 * str);
+extern void sub_80A103C(u8 taskId);
+extern void PrintNotTheTimeToUseThat(u8 taskId, bool8 inField);
+//extern void sub_80A1C08(u8 taskId);
+
+static void ItemUseOnFieldCB_Bike(u8 taskId);
+
+#define tUsingRegisteredKeyItem  data[3]
 
 void ItemUseOutOfBattle_Bike(u8 taskId)
 {
-    s16* data = gTasks[taskId].data;
-    s16 coordsY;
-    s16 coordsX;
+    s16 x, y;
     u8 behavior;
-    PlayerGetDestCoords(&coordsX, &coordsY);
-    behavior = MapGridGetMetatileBehaviorAt(coordsX, coordsY);
-    if (FlagGet(FLAG_SYS_CYCLING_ROAD) == TRUE || MetatileBehavior_IsVerticalRail(behavior) == TRUE || MetatileBehavior_IsHorizontalRail(behavior) == TRUE || MetatileBehavior_IsIsolatedVerticalRail(behavior) == TRUE || MetatileBehavior_IsIsolatedHorizontalRail(behavior) == TRUE)
-        DisplayCannotDismountBikeMessage(taskId, tUsingRegisteredKeyItem);
-    else
+
+    PlayerGetDestCoords(&x, &y);
+    behavior = MapGridGetMetatileBehaviorAt(x, y);
+
+    if (FlagGet(FLAG_SYS_ON_CYCLING_ROAD) == TRUE
+     || MetatileBehavior_IsVerticalRail(behavior) == TRUE
+     || MetatileBehavior_IsHorizontalRail(behavior) == TRUE
+     || MetatileBehavior_IsIsolatedVerticalRail(behavior) == TRUE
+     || MetatileBehavior_IsIsolatedHorizontalRail(behavior) == TRUE)
+        DisplayItemMessageInCurrentContext(taskId, gTasks[taskId].tUsingRegisteredKeyItem, 2, gUnknown_8416451);
+    else if (Overworld_IsBikingAllowed() == TRUE && !IsBikingDisallowedByPlayer() && FollowerCanBike())
     {
-        //if (Overworld_IsBikingAllowed() == TRUE && IsBikingDisallowedByPlayer() == 0)
-        if (Overworld_IsBikingAllowed() == TRUE && IsBikingDisallowedByPlayer() == 0 && FollowerCanBike())
-        {
-            sItemUseOnFieldCB = ItemUseOnFieldCB_Bike;
-            SetUpItemUseOnFieldCallback(taskId);
-        }
-        else
-            DisplayDadsAdviceCannotUseItemMessage(taskId, tUsingRegisteredKeyItem);
+        sItemUseOnFieldCB = ItemUseOnFieldCB_Bike;
+        sub_80A103C(taskId);
     }
+    else
+        PrintNotTheTimeToUseThat(taskId, gTasks[taskId].tUsingRegisteredKeyItem);
 }
+
+/**
+ * ::ACIMUT::
+ * 2022/04/07
+ * 
+ * - Esta función en FR se conoce como:
+ *      static void ItemUseOnFieldCB_Bicycle(u8 taskId)
+ * - Cambio de función correspondiente a fire red.
+ * - Es llamado en la función anterior.
+ * - No es necesario hacer hook.
+ */
+
 static void ItemUseOnFieldCB_Bike(u8 taskId)
 {
-    if (ItemId_GetSecondaryId(gSpecialVar_ItemId) == MACH_BIKE)
-        GetOnOffBike(PLAYER_AVATAR_FLAG_MACH_BIKE);
-    else
-        GetOnOffBike(PLAYER_AVATAR_FLAG_ACRO_BIKE);
+    if (!TestPlayerAvatarFlags(PLAYER_AVATAR_FLAG_MACH_BIKE | PLAYER_AVATAR_FLAG_ACRO_BIKE))
+        PlaySE(SE_BIKE_BELL);
+    GetOnOffBike(PLAYER_AVATAR_FLAG_MACH_BIKE | PLAYER_AVATAR_FLAG_ACRO_BIKE);
 
     FollowMe_HandleBike();
-    ScriptUnfreezeObjectEvents();
+    ClearPlayerHeldMovementAndUnfreezeObjectEvents();
     ScriptContext2_Disable();
     DestroyTask(taskId);
 }
 
+/**
+ * ::ACIMUT::
+ * 2022/04/06
+ * 
+ * - Esta función en FR se conoce como:
+ *      bool8 CanUseEscapeRopeOnCurrMap(void)
+ * - Cambio de función correspondiente a fire red.
+ * - Es llamada en otra parte de la inyección.
+ * - Es posible hacer hook a toda esta función.
+ */
 
 bool8 CanUseDigOrEscapeRopeOnCurMap(void)
 {
@@ -89,19 +136,22 @@ bool8 CanUseDigOrEscapeRopeOnCurMap(void)
     else
         return FALSE;
 }
+
+/*      //es necesario hacer nada aquí xd
 void ItemUseOutOfBattle_EscapeRope(u8 taskId)
 {
     if (CanUseDigOrEscapeRopeOnCurMap() == TRUE)
     {
-        sItemUseOnFieldCB = ItemUseOnFieldCB_EscapeRope;
-        SetUpItemUseOnFieldCallback(taskId);
+        ItemUse_SetQuestLogEvent(QL_EVENT_USED_ITEM, NULL, gSpecialVar_ItemId, gMapHeader.regionMapSectionId);
+        sItemUseOnFieldCB = sub_80A1C08;
+        sub_80A103C(taskId);
     }
     else
     {
-        DisplayDadsAdviceCannotUseItemMessage(taskId, gTasks[taskId].tUsingRegisteredKeyItem);
+        PrintNotTheTimeToUseThat(taskId, gTasks[taskId].tUsingRegisteredKeyItem);
     }
 }
-
+*/
 
 
 
