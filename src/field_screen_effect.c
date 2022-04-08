d0abe28e968d149d3ce063f2ca7a4de4dd23da9e
Acimut <79547805+Acimut@users.noreply.github.com>
2022-04-07 22:05:44 -0500

    field_screen_effect.c

--- src/field_screen_effect.c
+++ src/field_screen_effect.c
@@ -35,18 +35,34 @@
 //#include "constants/rgb.h"
 //#include "trainer_hill.h"
 #include "fldeff.h"
+#include "field_fadetransition.h"
+#include "quest_log.h"
+
 
 #define tState       data[0]
 
-//          más hooks
+//static
+extern void SetPlayerVisibility(bool8 visible);
+extern void WarpFadeInScreenWithDelay(void);
+extern void sub_807DE78(bool8 a0);
 
 
+
+/**
+ * ::ACIMUT::
+ * 2022/04/07
+ * 
+ * - Esta función en FR está en field_fadetransition.c
+ * - Cambio de función correspondiente a fire red.
+ * - No es llamada en otra parte de la inyección.
+ * - Hacer hook a toda esta función.
+ */
 
 void FieldCB_DefaultWarpExit(void)
 {
     Overworld_PlaySpecialMapMusic();
-    WarpFadeInScreen();
-    SetUpWarpExitTask();
+    QuestLog_DrawPreviouslyOnQuestHeaderIfInPlaybackMode();
+    sub_807DE78(FALSE);
 
     FollowMe_WarpSetEnd();
 
@@ -55,14 +71,35 @@ void FieldCB_DefaultWarpExit(void)
 
 
 
-static void Task_ExitDoor(u8 taskId)
+/**
+ * ::ACIMUT::
+ * 2022/04/07
+ * 
+ * - Esta función en FR está en field_fadetransition.c
+ * - Las siguiente función sólo es llamada en:
+ *              static void sub_807DE78(bool8 a0)
+ * 
+ * - Esta función en FR se conoce como:
+ *          static void sub_807DFBC(u8 taskId)
+ * - Cambio de función correspondiente a fire red.
+ * - No es llamada en otra parte de la inyección.
+ * 
+ * - Sólo hay que cambiar un puntero.
+ */
+
+//static
+void Task_ExitDoor(u8 taskId)
 {
-    struct Task *task = &gTasks[taskId];
+    struct Task * task = &gTasks[taskId];
     s16 *x = &task->data[2];
     s16 *y = &task->data[3];
+
+    if (task->tState == 0)
+        task->tState = 5;
+
     switch (task->tState)
     {
-    case 0:
+    case 0: // Never reached
         HideFollower();
         SetPlayerVisibility(FALSE);
         FreezeObjectEvents();
@@ -70,44 +107,97 @@ static void Task_ExitDoor(u8 taskId)
         FieldSetDoorOpened(*x, *y);
         task->tState = 1;
         break;
-    case 1:
-        if (WaitForWeatherFadeIn())
+    case 5:
+        HideFollower();
+        SetPlayerVisibility(FALSE);
+        FreezeObjectEvents();
+        DoOutwardBarnDoorWipe();
+        WarpFadeInScreenWithDelay();
+        task->tState = 6;
+        break;
+    case 6:
+        task->data[15]++;
+        if (task->data[15] == 25)
         {
-            u8 objEventId;
+            PlayerGetDestCoords(x, y);
+            PlaySE(GetDoorSoundEffect(*x, *y));
+            FieldAnimateDoorOpen(*x, *y);
+            task->tState = 7;
+        }
+        break;
+    case 7:
+        if (!FieldIsDoorAnimationRunning())
+        {
+            PlayerGetDestCoords(&task->data[12], &task->data[13]);
             SetPlayerVisibility(TRUE);
-            objEventId = GetObjectEventIdByLocalIdAndMap(OBJ_EVENT_ID_PLAYER, 0, 0);
-            ObjectEventSetHeldMovement(&gObjectEvents[objEventId], MOVEMENT_ACTION_WALK_NORMAL_DOWN);
+            ObjectEventSetHeldMovement(&gObjectEvents[GetObjectEventIdByLocalIdAndMap(OBJ_EVENT_ID_PLAYER, 0, 0)], MOVEMENT_ACTION_WALK_NORMAL_DOWN);
+            task->tState = 8;
+        }
+        break;
+    case 8:
+        task->data[14]++;
+        if (task->data[14] == 14)
+        {
+            FieldAnimateDoorClose(task->data[12], task->data[13]);
+            task->tState = 9;
+        }
+        break;
+    case 9:
+        if (FieldFadeTransitionBackgroundEffectIsFinished() && walkrun_is_standing_still() && !FieldIsDoorAnimationRunning() && !FuncIsActiveTask(Task_BarnDoorWipe))
+        {
+            ObjectEventClearHeldMovementIfFinished(&gObjectEvents[GetObjectEventIdByLocalIdAndMap(OBJ_EVENT_ID_PLAYER, 0, 0)]);
+            task->tState = 4;
+        }
+        break;
+    // Legacy RS
+    case 1:
+        if (FieldFadeTransitionBackgroundEffectIsFinished())
+        {
+            SetPlayerVisibility(TRUE);
+            ObjectEventSetHeldMovement(&gObjectEvents[GetObjectEventIdByLocalIdAndMap(OBJ_EVENT_ID_PLAYER, 0, 0)], MOVEMENT_ACTION_WALK_NORMAL_DOWN);
             task->tState = 2;
         }
         break;
     case 2:
         if (walkrun_is_standing_still())
         {
-            u8 objEventId;
             task->data[1] = FieldAnimateDoorClose(*x, *y);
-            objEventId = GetObjectEventIdByLocalIdAndMap(OBJ_EVENT_ID_PLAYER, 0, 0);
-            ObjectEventClearHeldMovementIfFinished(&gObjectEvents[objEventId]);
+            ObjectEventClearHeldMovementIfFinished(&gObjectEvents[GetObjectEventIdByLocalIdAndMap(OBJ_EVENT_ID_PLAYER, 0, 0)]);
             task->tState = 3;
         }
         break;
     case 3:
         if (task->data[1] < 0 || gTasks[task->data[1]].isActive != TRUE)
-        {
-            FollowMe_SetIndicatorToComeOutDoor();
-            FollowMe_WarpSetEnd();
-
-            UnfreezeObjectEvents();
             task->tState = 4;
-        }
         break;
     case 4:
+        FollowMe_SetIndicatorToComeOutDoor();
+        FollowMe_WarpSetEnd();
+        UnfreezeObjectEvents();
         ScriptContext2_Disable();
         DestroyTask(taskId);
         break;
     }
 }
 
-static void Task_ExitNonAnimDoor(u8 taskId)
+/**
+ * ::ACIMUT::
+ * 2022/04/07
+ * 
+ * - Esta función en FR está en field_fadetransition.c
+ * - Las siguiente función sólo es llamada en:
+ *              static void sub_807DE78(bool8 a0)
+ * 
+ * - Esta función en FR se conoce como:
+ *          static void task_map_chg_seq_0807E20C(u8 taskId)
+ * - Cambio de función correspondiente a fire red.
+ * - No es llamada en otra parte de la inyección.
+ * 
+ * - Sólo hay que cambiar un puntero.
+ */
+
+//static 
+void Task_ExitNonAnimDoor(u8 taskId)
 {
     struct Task *task = &gTasks[taskId];
     s16 *x = &task->data[2];
@@ -122,7 +212,7 @@ static void Task_ExitNonAnimDoor(u8 taskId)
         task->tState = 1;
         break;
     case 1:
-        if (WaitForWeatherFadeIn())
+        if (FieldFadeTransitionBackgroundEffectIsFinished())
         {
             u8 objEventId;
             SetPlayerVisibility(TRUE);
@@ -148,20 +238,35 @@ static void Task_ExitNonAnimDoor(u8 taskId)
     }
 }
 
+/**
+ * ::ACIMUT::
+ * 2022/04/07
+ * 
+ * - Esta función en FR está en field_fadetransition.c
+ * - Las siguiente función sólo es llamada en:
+ *              static void FieldCB_TeleportWarpIn(void)
+ * 
+ * - Esta función en FR se conoce como:
+ *          static void Task_TeleportWarpIn(u8 taskId)
+ * - Cambio de función correspondiente a fire red.
+ * - No es llamada en otra parte de la inyección.
+ * 
+ * - Sólo hay que cambiar un puntero.
+ */
 
-
-static void Task_SpinEnterWarp(u8 taskId)
+//static 
+void Task_SpinEnterWarp(u8 taskId)
 {
     switch (gTasks[taskId].tState)
     {
     case 0:
         FreezeObjectEvents();
         ScriptContext2_Enable();
-        DoPlayerSpinEntrance();
+        StartTeleportInPlayerAnim();
         gTasks[taskId].tState++;
         break;
     case 1:
-        if (WaitForWeatherFadeIn() && IsPlayerSpinEntranceActive() != TRUE)
+        if (FieldFadeTransitionBackgroundEffectIsFinished() && WaitTeleportInPlayerAnim() != TRUE)
         {
             FollowMe_WarpSetEnd();
 
