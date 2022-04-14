905ca7f24f9aaa6364ddea848daf945358f66b9b
thoru72 <85423501+thoru72@users.noreply.github.com>
2022-04-14 16:48:56 -0400

    Update party_menu_hook.s

--- hooks/BPRE/party_menu_hook.s
+++ hooks/BPRE/party_menu_hook.s
@@ -5,7 +5,7 @@
 //08124998 l 00000072 SetUpFieldMove_Surf
 //99 49 12 08   puntero hallado en: 0x0845A7A8
 
-org 0x0845A7A8
+.org 0x0845A7A8
     .word SetUpFieldMove_Surf|1
 
 
@@ -14,7 +14,7 @@ org 0x0845A7A8
 //08124a8c l 00000022 SetUpFieldMove_Fly
 //8d 4a 12 08   puntero hallado en: 0x0845A798
 
-org 0x0845A798
+.org 0x0845A798
     .word SetUpFieldMove_Fly|1
 
 
@@ -23,7 +23,7 @@ org 0x0845A798
 //08124af8 l 00000066 SetUpFieldMove_Waterfall
 //f9 4a 12 08   puntero hallado en: 0x0845A7B8
 
-org 0x0845A7B8
+.org 0x0845A7B8
     .word SetUpFieldMove_Waterfall|1
 
 
