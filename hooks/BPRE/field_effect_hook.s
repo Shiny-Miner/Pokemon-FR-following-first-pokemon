b3645b0703b0624079aa0f1ee3efd71fa7524d49
Acimut <79547805+Acimut@users.noreply.github.com>
2022-04-14 12:16:21 -0500

    field_effect_hook.s

---
+++ hooks/BPRE/field_effect_hook.s
@@ -0,0 +1,29 @@
+//  -------------------------------
+//  -----   field_effect.c    -----
+//  -------------------------------
+
+//08084708 l 0000007c FallWarpEffect_7
+//09 47 08 08   puntero hallado en: 0x083CC04C
+//función a llamar:
+//bool8 FallWarpEffect_End(struct Task *task)
+
+.org 0x083CC04C
+    .word FallWarpEffect_End|1
+
+
+
+//08084820 l 00000070 EscalatorWarpEffect_2
+//21 48 08 08   puntero hallado en: 0x083CC054
+//función a llamar:
+//bool8 EscalatorWarpOut_WaitForPlayer(struct Task *task)
+
+
+
+
+//08086ab4 l 0000007c UseSurfEffect_4
+//b5 6a 08 08   puntero hallado en: 0x083CC160
+//función a llamar:
+//static void SurfFieldEffect_JumpOnSurfBlob(struct Task *task)
+
+
+
